bbcd7b5687d02f9315dfc2aa68342816c96413d1
mathlover777 <souravmathlover@gmail.com>
2017-07-02 00:35:12 +0530

    frontend refactored

--- static/js/fuzzy_c_helper.js
+++ static/js/fuzzy_c_helper.js
@@ -1,8 +1,13 @@
 $('#run_algorithm').on('click', function (e) {
+	update_progress_bar("0",false);
 	var request_data_json = construct_api_request_object();
-	start_algorithm(request_data_json);
+	var cache_data = new Object();
+	cache_data.task_id_cache = "fuzzy_c_task_id";
+	cache_data.pid_cache = "fuzzy_c_pid";
+	cache_data.output_file_path = "";
+	cache_data.fake_percentage = false;
+	start_algorithm("/run-algo",request_data_json,cache_data);
 })
-
 
 function construct_api_request_object() {
 
@@ -29,63 +34,6 @@ function construct_api_request_object() {
 	append_to_log("Request JSON generated\n" + request_data_json);
 	update_cache_input_variables();
   	return request_data_json;
-}
-
-function update_cache_input_variables(){
-	localStorage['image_pickle_file_path'] = $("#image_pickle_file_path").val();
-	localStorage['output_path'] = $("#output_path").val();
-	return;
-}
-
-function start_algorithm(request_data_json) {
-	$.post("/run-algo",request_data_json,start_algorithm_callback,"text");
-	return;
-}
-
-function start_algorithm_callback(data,status) {
-	append_to_log("Response For Algorithm Call")
-	append_to_log("[" + status + "] " + data);
-	var data = JSON.parse(data);
-	localStorage['fuzzy_c_task_id'] = data.task_id;
-	$('#task_id').text(data.task_id);
-	poll_for_task(data.task_id);
-	return;
-}
-
-
-function poll_for_task(task_id) {
-	var poll_request = new Object();
-	poll_request.task_id = task_id;
-	$.post("/get-task-status",JSON.stringify(poll_request),poll_for_task_callback,"text");
-	return;
-}
-
-function poll_for_task_callback(data,status) {
-	append_to_log("Result of Task Poll")
-	append_to_log("[" + status + "] " + data);
-	var data = JSON.parse(data);
-	$('#pid').text(data.pid);
-	localStorage['fuzzy_c_pid'] = data.pid;
-	update_progress_bar(data.percentage_done,false);
-	if (!data.is_done){
-		setTimeout(function(){ poll_for_task(data.task_id); }, 2000);
-	}else{
-		update_progress_bar(100,false);
-	}
-	return;
-}
-
-function update_progress_bar(percentage_done,fake_percentage) {
-	if(!fake_percentage){
-		$("#progress_bar").css("width", percentage_done + "%");
-	}else{
-		var current_progress = parseFloat($("#progress_bar").width());
-		var total_progress = parseFloat($("#progress").width());
-		var current_fraction = current_progress / total_progress;
-		var new_progress = (1 - (1 - current_fraction)  / Math.pow(Math.E,0.4) ) * 90;
-		$("#progress_bar").css("width", new_progress + "%");
-	}
-	return;
 }
 
 function init() {
--- static/js/helper.js
+++ static/js/helper.js
@@ -24,9 +24,78 @@ function append_to_log(append_text) {
 	return
 }
 
-function append_to_log(append_text) {
-	var $api_log = $('#api_log');
-	$api_log.append(append_text + "\n********************\n");
-	$api_log.scrollTop($api_log[0].scrollHeight);
-	return
+function update_progress_bar(percentage_done,fake_percentage) {
+	if(!fake_percentage){
+		$("#progress_bar").css("width", percentage_done + "%");
+	}else{
+		var current_progress = parseFloat($("#progress_bar").width());
+		var total_progress = parseFloat($("#progress").width());
+		var current_fraction = current_progress / total_progress;
+		var new_progress = (1 - (1 - current_fraction)  / Math.pow(Math.E,0.4) ) * 90;
+		$("#progress_bar").css("width", new_progress + "%");
+	}
+	return;
+}
+
+function update_cache_input_variables(){
+	localStorage['image_pickle_file_path'] = $("#image_pickle_file_path").val();
+	localStorage['output_path'] = $("#output_path").val();
+	return;
+}
+
+function start_algorithm(url,request_data_json,cache_data) {
+	var start_algorithm_callback_wrapper = function(cache_data) {
+		return function (data,status) {
+			start_algorithm_callback(data,status,cache_data);
+		}
+	}
+	$.post(url,request_data_json,start_algorithm_callback_wrapper(cache_data),"text");
+	return;
+}
+
+function start_algorithm_callback(data,status,cache_data) {
+	var task_id_cache = cache_data.task_id_cache;
+	append_to_log("Response For Algorithm Call")
+	append_to_log("[" + status + "] " + data);
+	var data = JSON.parse(data);
+	localStorage[task_id_cache] = data.task_id;
+	$('#task_id').text(data.task_id);
+	poll_for_task(data.task_id,cache_data);
+	return;
+}
+
+function poll_for_task(task_id,cache_data) {
+	var poll_request = new Object();
+	poll_request.task_id = task_id;
+
+	var poll_for_task_callback_wrapper = function(cache_data) {
+		return function(data,status) {
+			poll_for_task_callback(data,status,cache_data);
+		}
+	}
+
+	$.post("/get-task-status", JSON.stringify(poll_request), poll_for_task_callback_wrapper(cache_data), "text");
+	return;
+}
+
+
+function poll_for_task_callback(data,status,cache_data) {
+	var pid_cache = cache_data.pid_cache;
+	var output_file_path = cache_data.output_file_path;
+	append_to_log("Result of Task Poll")
+	append_to_log("[" + status + "] " + data);
+	var data = JSON.parse(data);
+	$('#pid').text(data.pid);
+	localStorage[pid_cache] = data.pid;
+	update_progress_bar(data.percentage_done,cache_data.fake_percentage);
+	if (!data.is_done){
+		setTimeout(function(){ poll_for_task(data.task_id,cache_data); }, 2000);
+	}else{
+		if (output_file_path != ""){
+			$("#" + output_file_path).val(data.result_file_name);
+			localStorage[output_file_path] = data.result_file_name;
+		}
+		update_progress_bar(100,false);
+	}
+	return;
 }
