2503f159412c3604dc592e0c695d5958ebf2e2d0
djjayeeta <dj.jayeeta@gmail.com>
2017-07-01 23:02:14 +0530

    random centroids

--- algo/centroid_init.py
+++ algo/centroid_init.py
@@ -16,3 +16,26 @@ def scklearn_kmeans_centroids(data,cluster_number):
 	kmeans = KMeans(n_clusters=cluster_number, random_state=0).fit(X)
 	centroids = kmeans.cluster_centers_
 	return centroids.astype(dtype=np.float64)
+
+
+def random_centroids(data,cluster_number):
+	row_size = data.shape[0]
+	col_size = data.shape[1]
+	channel_count = data.shape[2]
+	m = 1.3
+	U = np.random.rand(row_size,col_size,cluster_number)
+	for i in xrange(0,row_size):
+		for j in xrange(0,col_size):
+			total_u = sum(U[i][j])
+			U[i][j] / total_u
+	V = np.zeros((cluster_number,channel_count))
+	for r in xrange(0,cluster_number):
+		normalizer = 0.0
+		cluster_center = np.zeros(channel_count)
+		for i in xrange(0,row_size):
+			for j in xrange(0,col_size):
+				cluster_center += (U[i][j][r]**m) * data[i][j]
+				normalizer += U[i][j][r]**m
+		V[r] = cluster_center / normalizer
+		
+	return V
