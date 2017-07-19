1ae66b0bc3e3921cd58839800f263875c1dc91d1
djjayeeta <dj.jayeeta@gmail.com>
2017-07-19 16:34:29 +0530

    api edits

--- algo/image_helper.py
+++ algo/image_helper.py
@@ -87,3 +87,10 @@ def save_image(L,output_path,colors):
 	im.save(output_path)
 	return	
 
+
+def get_spaced_colors(cluster_number): 
+	max_value = 16581375
+	interval = int(max_value / cluster_number)
+	colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
+	return [[int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)] for i in colors]
+
