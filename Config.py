0f275c3dbe40e658f7f0ff9873a62f13ada89280
Jayeeta Datta <jayeeta@dunzo.in>
2018-05-18 10:37:32 +0530

    salinas exp

--- Config.py
+++ Config.py
@@ -13,21 +13,26 @@ gpu = -1
 unlabeled_tag = 0
 default_positive_prior = 0.33
 PATCH_SIZE = 3
-nbands = 10
+nbands = 8
 # Data name Can be Indian_pines, Salinas, PaviaU
-data = "Indian_pines"
-url1 = "http://www.ehu.eus/ccwintco/uploads/2/22/Indian_pines.mat"
-url2 = "http://www.ehu.eus/ccwintco/uploads/c/c4/Indian_pines_gt.mat"
+# data = "Indian_pines"
+data = "Salinas"
+# For Salinas
+url1 = "http://www.ehu.eus/ccwintco/uploads/f/f1/Salinas.mat"
+url2 = "http://www.ehu.eus/ccwintco/uploads/f/fa/Salinas_gt.mat"
+# url1 = "http://www.ehu.eus/ccwintco/uploads/2/22/Indian_pines.mat"
+# url2 = "http://www.ehu.eus/ccwintco/uploads/c/c4/Indian_pines_gt.mat"
 
 """test type 1 config"""
 type_1_train_pos_labelled_percentage = 60
 # percentage of pixels from total pixels of positive class that will be included in training, type_1_train_pos_labelled_percentage% of positive pixels will be labelled
-type_1_train_pos_percentage = 10
+type_1_train_pos_percentage = 7
 # percentage of pixels from total pixels of negative class (include_class_list - positive_class) that will be included in training
 type_1_train_neg_percentage = 30 # not required
 # type_1_pos_neg_ratio_in_train = [1, 0.82, 0.67, 0.54, 0.43, 0.33, 0.25, 0.18]
 type_1_pos_neg_ratio_in_train = [1, 0.82, 0.67, 0.54, 0.43, 0.33, 0.25, 0.18]
-type_1_cross_pos_percentage = 7
+# type_1_pos_neg_ratio_in_train = [1]
+type_1_cross_pos_percentage = 5
 type_1_pos_neg_ratio_in_cross = 1
 """ 
 boolean value to indicate the way of selection for negative unlabelled training data,
@@ -40,8 +45,8 @@ is_random_neg = True
 
 # class label included in this test, negative class  = list(set(include_class_list) - set(positive_class))
 # Note: Class labels should be present in groundtooth image
-# include_class_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-type_1_include_class_list = [2, 3, 5, 6, 8, 10, 11, 12, 14]
+type_1_include_class_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+# type_1_include_class_list = [2, 3, 5, 6, 8, 10, 11, 12, 14]
     # [2, 0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     # [2, 3, 5, 6, 8, 10, 11, 12, 14]
 # type_1_include_class_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
