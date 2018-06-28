008031cbc0fa5f43681c0f7f6350e6e513b22482
Jayeeta Datta <jayeeta@dunzo.in>
2018-06-28 22:38:56 +0530

    useless file  cleaning

--- run_exp2.py
+++
@@ -1,9 +0,0 @@
-import os, django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HSISeg.settings")
-django.setup()
-from semiSuper.PU_exp import run as run_PU
-from semiSuper.exp_preprocessing import get_preprocessed_data
-
-clust_labelled_img, clust_prob_labelled_img, preprocessed_img, target_mat = get_preprocessed_data()
-
-run_PU(clust_labelled_img, preprocessed_img, target_mat)
